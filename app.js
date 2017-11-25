ae334888774b4435d9955c48ecb97eef37d98110
edin <osmedin@gmail.com>
2017-11-25 11:01:13 -0600

    First Part - Creating web servers with Node.js and Express

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var routes = require('./api/routes');
+
+
+app.set('port', 3000);
+
+app.use( function(req, res, next){
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended : false }))
+
+app.use('/api', routes);
+ 
+
+app.get('/json', function(req, res){
+    console.log("GET the json");
+    res 
+    .status(200)
+    .json({"jsonData" : true});
+});
+
+app.get('/file', function(req, res){
+    console.log("GET the file");
+    res 
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
+});
+
+var server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+    console.log('Magic happens on port '+port);    
+});
